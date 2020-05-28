11560577296a609959da2d3c59e37bb1ea5b4d86
ferrisbueller66 <github email address>
2020-05-27 22:12:06 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,13 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def status
+      unless self.active == true
+        "This student is currently inactive"
+      else
+        "This student is currently active"
+      end
+  end
+
 end
